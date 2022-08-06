448e20bca02d0d2209b19c812a712cd1ffa99183
sogun <sogun.kwn@gmail.com>
2022-08-07 05:04:23 +0800

    register post1

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        "/register/registerRequest",
-        "/register/registerResponse"
+        "register",
     ];
 }
