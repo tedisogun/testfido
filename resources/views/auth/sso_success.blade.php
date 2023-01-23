96d4814d04850cc0967dceefc1420f3362174d72
Sogun <sogun.kwn@gmail.com>
2023-01-23 13:55:44 +0800

    passkey shit 25

--- resources/views/auth/sso_success.blade.php
+++ resources/views/auth/sso_success.blade.php
@@ -68,6 +68,9 @@
     </style>
 
     <script src="/welcome_sso_files/OneSignalSDK.js" async=""></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <body>
 
 <!-- Navigation Bar-->
@@ -704,4 +707,5 @@ max-width: 215px;">
 <!-- App js -->
 <script src="/welcome_sso_files/jquery.core.js"></script>
 <script src="/welcome_sso_files/jquery.app.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js" integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body></html>
