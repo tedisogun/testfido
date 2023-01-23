63516d58320ab12dda7de675e2d9697ddf932b9c
Sogun <sogun.kwn@gmail.com>
2023-01-23 18:16:06 +0800

    start 5

--- resources/views/passkey/sso_login.blade.php
+++ resources/views/passkey/sso_login.blade.php
@@ -286,6 +286,7 @@
   ================================================ -->
 <!-- jQuery Library -->
 <script type="text/javascript" src="/login_sso_files/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js" integrity="sha512-3j3VU6WC5rPQB4Ld1jnLV7Kd5xr+cq9avvhwqzbH/taCRNURoeEpoPBK9pDyeukwSxwRPJ8fDgvYXd6SkaZ2TA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!--materialize js-->
 <script type="text/javascript" src="/login_sso_files/materialize.min.js"></script>
 <!--scrollbar-->
